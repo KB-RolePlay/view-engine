dc6285f9d94c13ac58ca92915e1902aae1930c0c
Hugo H. <51268820+hugoheml@users.noreply.github.com>
2025-08-30 22:58:45 +0200

    feat: remove pug

--- deps.ts
+++ deps.ts
@@ -8,4 +8,3 @@ export { Application } from "@oak/oak";
 
 export * as eta from "eta";
 export * as dejs from "dejs";
-export * as pug from "pug";
--- mod.ts
+++ mod.ts
@@ -1,7 +1,6 @@
 //! engines
 export { dejsEngine } from "./lib/engines/dejs/dejs.engine.ts";
 export { etaEngine } from "./lib/engines/eta/eta.engine.ts";
-export { pugEngine } from "./lib/engines/pug/pug.engine.ts";
 
 //! type
 export type { Engine, Adapter, ViewConfig } from "./lib/viewEngine.type.ts";
