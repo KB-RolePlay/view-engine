7e6052341f61063597c774395db3521efbe176cd
Hugo H. <51268820+hugoheml@users.noreply.github.com>
2025-08-30 23:08:29 +0200

    fix(deps): ts syntaxies

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
 export { existsSync } from "jsr:@std/fs@^1.0.19";
-export * as path from npm:path@^0.12.7;
+export * as path from "npm:path@^0.12.7";
 
 // test dependencies
 export { green, blue } from "jsr:@std/fmt@^1.0.8/colors";
 
 export { Application } from "jsr:@oak/oak@^17.1.6";
 
-export * as eta from jsr:@std/testing@^1.0.15;
+export * as eta from "jsr:@std/testing@^1.0.15";
